e8d0c5254404ff0f1f04ed5f3675121183ff7b9a
Yanick <yayey.christen@gmail.com>
2021-05-18 11:33:50 +0200

    added setAlert

--- telegramResponse.py
+++ telegramResponse.py
@@ -4,7 +4,7 @@ import config
 def simple_responses(input_text):
     user_message = str(input_text).lower()
 
-    if user_message in ("/showcoins"):
+    if user_message == "/showcoins":
         message = "btc, eth, bnb, uni, vet, iota, btt, icx, ada, dot, doge, xrp"
         return message
 
@@ -13,13 +13,27 @@ def simple_responses(input_text):
         user_message = user_message.replace("/btc", "")
         return user_message
 
-    if user_message.find("/showvalue") != -1:
-        user_message = user_message.replace("/showvalue ", "")
+    if user_message.find("/show") != -1:
+        user_message = user_message.replace("/show ", "")
         client = Client(config.BINANCE_API_KEY, config.BINANCE_SECRET_API_KEY)
-        crypto_price = client.get_symbol_ticker(symbol=user_message.upper())
-        print("test3")
+        crypto_price = client.get_symbol_ticker(symbol=user_message.upper() + "USDT")
         roundedPrice = str(round(float(crypto_price.get('price')), 2))
-        print("test4")
-        return roundedPrice
+        return roundedPrice + "$"
+
+    if user_message in ("/exit"):
+        exit()
+        return ''
+
+    if user_message.find("/setalert") != -1:
+        user_message = user_message.replace("/setalert ", "")
+        messages = user_message.split()
+        client = Client(config.BINANCE_API_KEY, config.BINANCE_SECRET_API_KEY)
+        crypto_price = client.get_symbol_ticker(symbol=messages[0].upper() + "USDT")
+        roundedPrice = round(float(crypto_price.get('price')), 2)
+        way = 'test'
+        print(roundedPrice)
+        print(messages[1])
+        way = ('under', 'over') [roundedPrice >= float(messages[1])]
+        return messages[0] + ' is ' + way + ' your number'
 
     return "Who the fuck is that guy"
